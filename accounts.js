963a41058feb33a54c5361380df4a76d4456af16
adishthapa <adishthapa@outlook.com>
2019-06-25 19:43:07 -0400

    Got basic functionality of user accounts to work

---
+++ accounts.js
@@ -0,0 +1,48 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCrrcl0Qk0lbbLwURSemFTE499mmFSVbvM",
+    authDomain: "project-01-704ce.firebaseapp.com",
+    databaseURL: "https://project-01-704ce.firebaseio.com",
+    projectId: "project-01-704ce",
+    storageBucket: "",
+    messagingSenderId: "638882612611",
+    appId: "1:638882612611:web:2566b2b54d0668d4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Create a variable to reference the database
+var database = firebase.database();
+// Link to Firebase Database for tracking users
+var users = database.ref("/users");
+
+// Function for signing up
+$("#signup-submit").click(function(event) {
+    event.preventDefault();
+    var name = $("#name-input").val().trim();
+    var email = $("#email-input").val().trim();
+    var password = $("#password-input").val().trim();
+
+    users.push({
+        name,
+        email,
+        password
+    });
+
+});
+
+// Function for logging in
+$("#login-submit").click(function(event) {
+    event.preventDefault();
+    var email = $("#email-input").val().trim();
+    var password = $("#password-input").val().trim();
+
+    users.orderByChild("email").equalTo(email).on("child_added", function(snapshot) {
+        if(snapshot.key) {
+            if (password === snapshot.val().password) {
+                $("body").html("Hello " + snapshot.val().name);
+            }  
+        }
+    });
+
+})
