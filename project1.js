d7e6311a1a52e9a18f2ffdabee1e5fd71a9d130c
adishthapa <adishthapa@outlook.com>
2019-06-27 20:40:31 -0400

    Started populating project1.js with html

---
+++ project1.js
@@ -0,0 +1,95 @@
+var queryURL = "https://data.opendatasoft.com/api/records/1.0/search/?dataset=open-beer-database%40public-us&rows=100&sort=name&facet=style_name&facet=cat_name&facet=name_breweries&facet=country&refine.country=United+States&refine.state=North+Carolina"
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    var beerObj = {};
+
+    for (var i = 0; i < response.records.length; i++) {
+        var city = response.records[i].fields.city;
+        var brewery = response.records[i].fields.name_breweries;
+        var beerName = response.records[i].fields.name;
+        var beerStyle = response.records[i].fields.style_name;
+        var beerCateg = response.records[i].fields.cat_name;
+        var beerABV = response.records[i].fields.abv;
+        var beerDescr = response.records[i].fields.descript;
+        var longitude = response.records[i].geometry.coordinates[0];
+        var latitude = response.records[i].geometry.coordinates[1];
+        if (!beerObj[city]) {
+            var cityInfo = {
+                [brewery]: {
+                    beers: [beerName],
+                    location: {
+                        longitude,
+                        latitude
+                    },
+                    [beerName]: {
+                        name: beerName,
+                        style: beerStyle,
+                        category: beerCateg,
+                        ABV: beerABV,
+                        description: beerDescr
+                    }
+                }
+            };
+            beerObj[city] = cityInfo;
+        } else if (!beerObj[city][brewery]) {
+            var beerInfo = {
+                name: beerName,
+                style: beerStyle,
+                category: beerCateg,
+                ABV: beerABV,
+                description: beerDescr
+            };
+            var breweryInfo = {
+                beers: [beerName],
+                location: {
+                    longitude,
+                    latitude
+                }
+            };
+   
+            beerObj[city][brewery] = breweryInfo;
+            breweryInfo[beerName] = beerInfo;
+        } else if (beerObj[city][brewery]["beers"].indexOf(beerName) === -1) {
+            beerObj[city][brewery]["beers"].push(beerName);
+            var beerInfo = {
+                name: beerName,
+                style: beerStyle,
+                category: beerCateg,
+                ABV: beerABV,
+                description: beerDescr
+            };
+            beerObj[city][brewery][beerName] = beerInfo;
+        }
+    }
+   
+    function fillBreweries(beerObj) {
+        var citiesList = Object.keys(beerObj).sort();
+        for (var i = 0; i < citiesList.length; i++) {
+            var newPanel = $("<div>").addClass("panel col-md-3")
+            var newPanelHeading = $("<div>").addClass("panel-heading")
+            var newPanelTitle = $("<div>").addClass("panel-title")
+            var newCity = $("<a>").attr("data-toggle", "collapse").attr("href", "#")
+            var cityName = citiesList[i]
+            newCity.text(cityName);
+
+            var breweriesList = Object.keys(beerObj[cityName]).sort();
+
+            for (var j = 0; j < breweriesList.length; j++) {
+                newBrewery = breweriesList[j];
+                // var newBrewery =
+            }
+            console.log(breweriesList);
+
+            newPanelTitle.append(newCity);
+            newPanelHeading.append(newPanelTitle);
+            newPanel.append(newPanelHeading);
+            $("#addHere").append(newPanel);
+        }
+
+    }
+
+    fillBreweries(beerObj)
+});
