10954874796ea4d8a38125ad7c89a372fc888323
adishthapa <adishthapa@outlook.com>
2019-07-02 00:42:05 -0400

    Finished implementing the favorite system

--- final product/js/app.js
+++ final product/js/app.js
@@ -1,8 +1,79 @@
 $(document).ready(function(){
 
+    var firebaseConfig = {
+        apiKey: "AIzaSyCrrcl0Qk0lbbLwURSemFTE499mmFSVbvM",
+        authDomain: "project-01-704ce.firebaseapp.com",
+        databaseURL: "https://project-01-704ce.firebaseio.com",
+        projectId: "project-01-704ce",
+        storageBucket: "",
+        messagingSenderId: "638882612611",
+        appId: "1:638882612611:web:2566b2b54d0668d4"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    // Create a variable to reference the database
+    var database = firebase.database();
+    // Link to Firebase Database for tracking users
+    var users = database.ref("/users");
+
     var queryURL = "https://data.opendatasoft.com/api/records/1.0/search/?dataset=open-beer-database%40public-us&rows=100&sort=name&facet=style_name&facet=cat_name&facet=name_breweries&facet=country&refine.country=United+States&refine.state=North+Carolina";
 
     var beerObj = {};
+
+    var loginStatus;
+    var user;
+    var favoriteBreweries = [];
+
+    // Function for signing up
+    $("#signup-submit").click(function(event) {
+        event.preventDefault();
+        var name = $("#signup-name-input").val().trim();
+        var email = $("#signup-email-input").val().trim();
+        var password = $("#signup-password-input").val().trim();
+
+        users.push({
+            name,
+            email,
+            password,
+            favoriteBreweries
+        });
+
+        $("#signup-name-input").val("");
+        $("#signup-email-input").val("");
+        $("#signup-password-input").val("");
+
+    });
+
+    // Function for logging in
+    $("#login-submit").click(function(event) {
+        event.preventDefault();
+        var email = $("#login-email-input").val().trim();
+        var password = $("#login-password-input").val().trim();
+        
+
+        users.orderByChild("email").equalTo(email).on("child_added", function(snapshot) {
+            if(snapshot.key) {
+                
+                if (password === snapshot.val().password) {
+                    
+                    loginStatus = true;
+                    user = snapshot.key;
+
+                    if (snapshot.val().favoriteBreweries) {
+                        favoriteBreweries = snapshot.val().favoriteBreweries;
+                    }
+                    $(".initial-log-in").hide();
+                    $("#home-buttons").append("<li><a class='initial-log-in href='#'>Signout</a></li>");
+                    $("#modal-login").modal("close");
+                }
+                
+            }
+        });
+    })
+
+    // Function for checking for favorite
+
 
     function createObj(response) {
            
@@ -137,15 +208,28 @@ $(document).ready(function(){
         createObj(response)
         
         fillBreweries(beerObj)
+
+        var currentBrewery;
     
         $(".brewery-link").on("click", function() {
             returned = beerObj[$(this).attr("id")][$(this).val()];
+            currentBrewery = returned;
             var brewNameDiv = $("<h1>").attr("id", "breweryName").text(returned.breweryName);
+            console.log(favoriteBreweries);
+            if (loginStatus) {
+                if(favoriteBreweries.indexOf(returned.breweryName) === -1 ) {
+                    brewNameDiv.append("&nbsp&nbsp&nbsp<a class='btn-floating black'><i class='medium material-icons favorite'>star_border</i></a>");
+                } else {
+                    brewNameDiv.append("&nbsp&nbsp&nbsp<a class='btn-floating yellow'><i class='medium material-icons favorite'>star</i></a>");
+                }
+            } else {
+                brewNameDiv.append("&nbsp&nbsp&nbsp<a class='btn-floating black'><i class='medium material-icons favorite'>star_border</i></a>");
+            }
             var brewAddressDiv = $("<h6>").attr("id", "breweryAddress").text(returned.address);
             var breweryLink = $("<a>").attr("href", returned.website).attr("target", "_blank").text(returned.website);
             var brewLinkDiv = $("<div>").attr("id", "breweryLink").append(breweryLink);
     
-            $("header").append(brewNameDiv, brewLinkDiv);
+            $("#beerInfoHeader").append(brewNameDiv);
             $("#addressDiv").append(brewAddressDiv);
     
             var beerList = returned.beers;
@@ -166,11 +250,39 @@ $(document).ready(function(){
             initMap(brewLat, brewLong);
 
         });
+
+        $(document).on("click", ".favorite", function() {
+            if (loginStatus) {
+                if (favoriteBreweries.indexOf(currentBrewery.breweryName) === -1) {
+                    $("#beerInfoHeader").empty();
+                    var brewNameDiv = $("<h1>").attr("id", "breweryName").text(currentBrewery.breweryName);
+                    brewNameDiv.append("&nbsp&nbsp&nbsp<a class='btn-floating yellow'><i class='medium material-icons favorite'>star</i></a>");
+                    favoriteBreweries.push(currentBrewery.breweryName);
+                    database.ref("/users/" + user).update( {
+                        favoriteBreweries: favoriteBreweries
+                    });
+                    $("#beerInfoHeader").append(brewNameDiv);
+                } else {
+                    $("#beerInfoHeader").empty();
+                    var brewNameDiv = $("<h1>").attr("id", "breweryName").text(currentBrewery.breweryName);
+                    brewNameDiv.append("&nbsp&nbsp&nbsp<a class='btn-floating black'><i class='medium material-icons favorite'>star_border</i></a>");
+                    favoriteBreweries.splice(currentBrewery.breweryName, 1);
+                    database.ref("/users/" + user).update( {
+                        favoriteBreweries: favoriteBreweries
+                    });
+                    $("#beerInfoHeader").append(brewNameDiv);
+                }
+                
+            } else {
+                $("#loginStatus").text("You must be logged in to add a brewery to your favorite list!");
+            }
+        });
     
         $(".modal-close").on("click", function() {
             $("#beerInfoHeader").empty();
             $("#beerInfoBody").empty();
             $("#addressDiv").empty();
+            $("#loginStatus").empty();
         });
 
     });
